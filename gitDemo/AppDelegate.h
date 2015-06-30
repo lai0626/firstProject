eced3ab54b3cc1611707156cbd41ff981a014540
lai <lai@laideMac-Pro.local>
2015-06-30 09:45:25 +0800

    Initial Commit

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by lai on 15/6/30.
+//  Copyright (c) 2015年 laiweihuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by lai on 15/6/30.
+//  Copyright (c) 2015年 laiweihuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
