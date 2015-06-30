8e941b6dfb1823e38716ac67323d0e5f33c7dec3
lai <lai@laideMac-Pro.local>
2015-06-30 09:46:39 +0800

    gitTest

--- gitDemo/ViewController.m
+++ gitDemo/ViewController.m
@@ -19,9 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)gitTest{
+    NSLog(@"gitTest");
 }
 
 @end
